7f46f37b1d0926ed08413aec5678e2b58e159c8d
Lion007 <yinxiang2016@gmail.com>
2020-03-01 20:11:52 +0800

    Update dependencies

--- app/src/main/java/com/github/filter/MainActivity.kt
+++ app/src/main/java/com/github/filter/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.github.filter
 
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var mGLSurfaceView: GLSurfaceView? = null
+    var colorMatrix: ColorMatrix? = null
+    var colorMatrixColorFilter: ColorMatrixColorFilter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
