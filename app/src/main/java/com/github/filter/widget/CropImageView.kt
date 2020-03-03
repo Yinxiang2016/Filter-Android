f3fc1d35dc110405d355c05bc840c916bc747d7b
Lion007 <yinxiang2016@gmail.com>
2020-03-03 17:21:32 +0800

    update CropImageView

--- app/src/main/java/com/github/filter/widget/CropImageView.kt
+++ app/src/main/java/com/github/filter/widget/CropImageView.kt
@@ -1,34 +1,140 @@
 package com.github.filter.widget
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import com.github.filter.R
 
 /**
  * @author Lion007（yinxiang2016@gmail.com）
- * @date 2020/3/2
+ * @date 2020/3/3 9:23 AM
  * Description:
  */
 
-class CropImageView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
-    defStyleRes: Int = 0
-) : AppCompatImageView(context, attrs, defStyleAttr) {
+class CropImageView : AppCompatImageView {
+
+    private var _exampleString: String? = null
+    private var _exampleColor: Int = Color.RED
+    private var _exampleDimension: Float = 0f
+
+    private var textPaint: TextPaint? = null
+    private var textWidth: Float = 0f
+    private var textHeight: Float = 0f
 
 
-//    private val boundedWidth: Int
-//    private val boundedHeight: Int
+    var exampleString: String?
+        get() = _exampleString
+        set(value) {
+            _exampleString = value
+            invalidateTextPaintAndMeasurements()
+        }
 
-    init {
-//        val arr = context.obtainStyledAttributes(attrs, R.styleable.BoundedGridLayout, defStyleAttr, defStyleRes)
-//        boundedWidth = arr.getDimensionPixelSize(R.styleable.BoundedGridLayout_bounded_width, 0)
-//        boundedHeight = arr.getDimensionPixelSize(R.styleable.BoundedGridLayout_bounded_height, 0)
-//        arr.recycle()
+    var exampleColor: Int
+        get() = _exampleColor
+        set(value) {
+            _exampleColor = value
+            invalidateTextPaintAndMeasurements()
+        }
+
+    var exampleDimension: Float
+        get() = _exampleDimension
+        set(value) {
+            _exampleDimension = value
+            invalidateTextPaintAndMeasurements()
+        }
+
+    var exampleDrawable: Drawable? = null
+
+    constructor(context: Context) : super(context) {
+        init(null, 0)
     }
 
-
-    private fun initImage(){
-
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
     }
 
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        // Load attributes
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.CropImageView, defStyle, 0
+        )
+
+        _exampleString = a.getString(
+            R.styleable.CropImageView_exampleString
+        )
+        _exampleColor = a.getColor(
+            R.styleable.CropImageView_exampleColor,
+            exampleColor
+        )
+        _exampleDimension = a.getDimension(
+            R.styleable.CropImageView_exampleDimension,
+            exampleDimension
+        )
+
+        if (a.hasValue(R.styleable.CropImageView_exampleDrawable)) {
+            exampleDrawable = a.getDrawable(
+                R.styleable.CropImageView_exampleDrawable
+            )
+            exampleDrawable?.callback = this
+        }
+        a.recycle()
+        textPaint = TextPaint().apply {
+            flags = Paint.ANTI_ALIAS_FLAG
+            textAlign = Paint.Align.LEFT
+        }
+        invalidateTextPaintAndMeasurements()
+    }
+
+    private fun invalidateTextPaintAndMeasurements() {
+        textPaint?.let {
+            it.textSize = exampleDimension
+            it.color = exampleColor
+            textWidth = it.measureText(exampleString)
+            textHeight = it.fontMetrics.bottom
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val paddingLeft = paddingLeft
+        val paddingTop = paddingTop
+        val paddingRight = paddingRight
+        val paddingBottom = paddingBottom
+
+        val contentWidth = width - paddingLeft - paddingRight
+        val contentHeight = height - paddingTop - paddingBottom
+
+        exampleString?.let {
+            // Draw the text.
+            canvas.drawText(
+                it,
+                paddingLeft + (contentWidth - textWidth) / 2,
+                paddingTop + (contentHeight + textHeight) / 2,
+                textPaint
+            )
+        }
+
+        // Draw the example drawable on top of the text.
+        exampleDrawable?.let {
+            it.setBounds(
+                paddingLeft, paddingTop,
+                paddingLeft + contentWidth, paddingTop + contentHeight
+            )
+            it.draw(canvas)
+        }
+    }
 }
