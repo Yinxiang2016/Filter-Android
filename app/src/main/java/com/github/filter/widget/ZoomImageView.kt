2c01049bd028fd4e2299989c8e3b1512117239ba
Lion007 <yinxiang2016@gmail.com>
2020-03-23 17:41:29 +0800

    update

---
+++ app/src/main/java/com/github/filter/widget/ZoomImageView.kt
@@ -0,0 +1,44 @@
+package com.github.filter.widget
+
+/**
+ * @author xiang.yin.o（xiang.yin.o@nio.com）
+ * @date 2020/3/23
+ * Description: 可以根据手势放大缩小的ImageView
+ *
+ * 边界
+ */
+
+
+class ZoomImageView {
+
+
+    /**
+     * 双击
+     */
+    fun doubleClick() {
+
+    }
+
+    /**
+     * 单击
+     */
+    fun singleClick() {
+
+    }
+
+    /**
+     * 缩小
+     */
+    fun zoomOut() {
+
+    }
+
+    /**
+     * 放大
+     */
+    fun zoomIn() {
+
+    }
+
+
+}
